2b0e935285a164010f7bfeded5a4b4038a225f28
kkhetarpal <khimyakhetarpal@gmail.com>
2016-02-13 22:32:47 -0500

    Reverse a string - non efficient

--- DS/src/Array.cpp
+++ DS/src/Array.cpp
@@ -3,7 +3,7 @@
 // Author      : Khimya
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description :Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
 //============================================================================
 
 #include <iostream>
@@ -25,14 +25,36 @@ bool isuniquechar(string str){
 	return true;
 }
 
+void reverseastring(string str){
+
+	int n = str.length();
+	string newstring;
+	if(n == 0)
+		cout << "Empty String" << endl;
+	if(n==1 || n==2)
+		cout << "The reverse string is " << str << endl;
+
+	for(int i = 0; i <= n-1; i++){
+		newstring[i] = str[n-i-1];
+		cout << newstring[i];
+	}
+}
+
+
 int main() {
 
 	string s1 =  "Khimya";
 	string s2 = "Priti";
 
-	bool s11 = isuniquechar(s1);
-	bool s22 = isuniquechar(s2);
-	cout << "Unique Characters in %s" << s1 << " ? " << s11  << endl; // prints !!!Hello World!!!
-	cout << "Unique Characters in %s" << s2 << " ? " << s22  << endl; // prints !!!Hello World!!!
+//	bool s11 = isuniquechar(s1);
+//	bool s22 = isuniquechar(s2);
+//	cout << "Unique Characters in %s" << s1 << " ? " << s11  << endl;
+//	cout << "Unique Characters in %s" << s2 << " ? " << s22  << endl;
+
+	reverseastring(s1);
+	cout << endl;
+	reverseastring(s2);
+	cout << endl;
+
 	return 0;
 }
